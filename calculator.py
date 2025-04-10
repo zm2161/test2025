e858a5ccff20b6d5c70128314c86e4b62b763fc4
zm2161 <zm2161@nyu.edu>
2025-04-10 09:35:29 -0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,4 @@
+# file: calculator.py
+
+def add(a, b):
+    return a + b
---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+# file: test_calculator.py
+
+from calculator import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
